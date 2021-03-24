ae5e04de81dc661c3287f36ba3a393277b427a84
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2021-03-24 17:29:54 +0100

    Add ping endpoint

---
+++ src/main/kotlin/no/nav/pensjon/selvbetjening/fssgw/tech/ping/PingController.kt
@@ -0,0 +1,21 @@
+package no.nav.pensjon.selvbetjening.fssgw.tech.ping
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class PingController {
+
+    @GetMapping("ping")
+    fun ping(): ResponseEntity<String> {
+        val httpHeaders = HttpHeaders()
+        httpHeaders.contentType = MediaType.APPLICATION_JSON
+        return ResponseEntity("{ \"reponse\": \"pong\" }", httpHeaders, HttpStatus.OK)
+    }
+}
---
+++ src/test/kotlin/no/nav/pensjon/selvbetjening/fssgw/tech/ping/PingControllerTest.kt
@@ -0,0 +1,27 @@
+package no.nav.pensjon.selvbetjening.fssgw.tech.ping
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@WebMvcTest(PingController::class)
+internal class PingControllerTest {
+
+    private val path = "/api/"
+
+    @Autowired
+    private val mvc: MockMvc? = null
+
+    @Test
+    fun ping() {
+        mvc!!.perform(get(path + "ping"))
+                .andExpect(status().isOk)
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(content().json("{ 'reponse': 'pong' }"))
+    }
+}
