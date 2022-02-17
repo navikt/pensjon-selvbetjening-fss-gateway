1ad7812ca2ccd3e2222b75961497361344721ba2
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2022-02-17 14:54:17 +0100

    PL-3664 Controller for tjenestepensjon

---
+++ src/main/kotlin/no/nav/pensjon/selvbetjening/fssgw/tjenestepensjon/TjenestepensjonController.kt
@@ -0,0 +1,35 @@
+package no.nav.pensjon.selvbetjening.fssgw.tjenestepensjon
+
+import no.nav.pensjon.selvbetjening.fssgw.common.ControllerBase
+import no.nav.pensjon.selvbetjening.fssgw.common.ServiceClient
+import no.nav.pensjon.selvbetjening.fssgw.tech.jwt.JwsValidator
+import no.nav.pensjon.selvbetjening.fssgw.tech.sts.ServiceTokenGetter
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
+
+@RestController
+@RequestMapping("api/tjenestepensjon")
+class TjenestepensjonController (
+    jwsValidator: JwsValidator,
+    egressTokenGetter: ServiceTokenGetter,
+    serviceClient: ServiceClient,
+    @Value("\${tp.url}") egressEndpoint: String) :
+    ControllerBase(jwsValidator, serviceClient, egressTokenGetter, egressEndpoint) {
+
+    @GetMapping("{pid}/haveYtelse")
+    fun handleGetRequest(request: HttpServletRequest): ResponseEntity<String> {
+        return super.doGet(request)
+    }
+
+    override fun egressAuthWaived(): Boolean {
+        return false
+    }
+
+    override fun consumerTokenRequired(): Boolean {
+        return false
+    }
+}
