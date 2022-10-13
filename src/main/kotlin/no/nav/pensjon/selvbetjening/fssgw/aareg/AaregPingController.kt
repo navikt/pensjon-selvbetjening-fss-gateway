c3c4d199b4968ea1cf8e0a7a5327c7025293a180
Sindre Rasmussen <sindre.rasmussen@nav.no>
2022-10-13 15:26:14 +0200

    PL-4017: Revert making AaregPingController protected.

--- src/main/kotlin/no/nav/pensjon/selvbetjening/fssgw/aareg/AaregPingController.kt
+++ src/main/kotlin/no/nav/pensjon/selvbetjening/fssgw/aareg/AaregPingController.kt
@@ -1,9 +1,7 @@
 package no.nav.pensjon.selvbetjening.fssgw.aareg
 
-import no.nav.pensjon.selvbetjening.fssgw.common.ProtectedControllerBase
 import no.nav.pensjon.selvbetjening.fssgw.common.ServiceClient
-import no.nav.pensjon.selvbetjening.fssgw.tech.jwt.JwsValidator
-import no.nav.pensjon.selvbetjening.fssgw.tech.sts.ServiceTokenGetter
+import no.nav.pensjon.selvbetjening.fssgw.common.UnprotectedControllerBase
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,22 +12,12 @@ import javax.servlet.http.HttpServletRequest
 @RestController
 @RequestMapping("aareg-services")
 class AaregPingController(
-        jwsValidator: JwsValidator,
-        egressTokenGetter: ServiceTokenGetter,
         serviceClient: ServiceClient,
         @Value("\${aareg.url}") egressEndpoint: String) :
-        ProtectedControllerBase(jwsValidator, egressTokenGetter, serviceClient, egressEndpoint) {
+        UnprotectedControllerBase(serviceClient, egressEndpoint) {
 
     @GetMapping("/api/ping")
     fun handleGetRequest(request: HttpServletRequest): ResponseEntity<String> {
         return super.doGet(request)
-    }
-
-    override fun egressAuthWaived(): Boolean {
-        return false
-    }
-
-    override fun consumerTokenRequired(): Boolean {
-        return true
     }
 }
