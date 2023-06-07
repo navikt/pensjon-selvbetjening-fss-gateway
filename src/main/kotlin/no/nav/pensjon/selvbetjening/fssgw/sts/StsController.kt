58c47913d798c602525d6aea6447f4c4426aba3b
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2023-06-07 11:00:44 +0200

    Support Gandalf STS token exchange (SAML)

--- src/main/kotlin/no/nav/pensjon/selvbetjening/fssgw/sts/StsController.kt
+++ src/main/kotlin/no/nav/pensjon/selvbetjening/fssgw/sts/StsController.kt
@@ -6,10 +6,7 @@ import no.nav.pensjon.selvbetjening.fssgw.common.ServiceClient
 import no.nav.pensjon.selvbetjening.fssgw.tech.jwt.JwsValidator
 import no.nav.pensjon.selvbetjening.fssgw.tech.sts.ServiceTokenGetter
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
 
 @RestController
@@ -24,11 +21,10 @@ class StsController(
     ingressTokenValidator, serviceClient, callIdGenerator, egressEndpoint, egressTokenGetter) {
 
     @GetMapping("token")
-    fun handleGetRequest(request: HttpServletRequest): ResponseEntity<String> {
-        return super.doGet(request)
-    }
+    fun handleGetRequest(request: HttpServletRequest) = super.doGet(request)
 
-    override fun consumerTokenRequired(): Boolean {
-        return false
-    }
+    @PostMapping("token/exchange")
+    fun handlePostRequest(@RequestBody body: String, request: HttpServletRequest) = super.doPost(request, body)
+
+    override fun consumerTokenRequired() = false
 }
--- src/test/kotlin/no/nav/pensjon/selvbetjening/fssgw/sts/StsControllerTest.kt
+++ src/test/kotlin/no/nav/pensjon/selvbetjening/fssgw/sts/StsControllerTest.kt
@@ -5,9 +5,9 @@ import no.nav.pensjon.selvbetjening.fssgw.common.ServiceClient
 import no.nav.pensjon.selvbetjening.fssgw.mock.MockUtil.serviceTokenData
 import no.nav.pensjon.selvbetjening.fssgw.tech.jwt.JwsValidator
 import no.nav.pensjon.selvbetjening.fssgw.tech.sts.ServiceTokenGetter
+import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Test
-import org.mockito.ArgumentMatchers.anyMap
-import org.mockito.ArgumentMatchers.anyString
+import org.mockito.ArgumentMatchers.*
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -15,6 +15,7 @@ import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.HttpHeaders
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest(StsController::class)
@@ -36,26 +37,72 @@ internal class StsControllerTest {
     lateinit var callIdGenerator: CallIdGenerator
 
     @Test
-    fun `token request results in JSON response`() {
+    fun `access token request results in JWT access token response`() {
         `when`(serviceClient.doGet(anyString(), anyMap()))
-            .thenReturn("""{ "response": "bar"}""")
+            .thenReturn(JWT_ACCESS_TOKEN_RESPONSE_BODY)
         `when`(egressTokenGetter.getServiceUserToken()).thenReturn(serviceTokenData())
 
         mvc.perform(
-            get(TOKEN_PATH)
+            get(JWT_ACCESS_TOKEN_PATH)
                 .header(HttpHeaders.AUTHORIZATION, "Bearer jwt"))
             .andExpect(status().isOk)
-            .andExpect(content().json("""{ "response": "bar"}"""))
+            .andExpect(content().json(JWT_ACCESS_TOKEN_RESPONSE_BODY))
     }
 
     @Test
-    fun `unauthorized token request results in response status Unauthorized`() {
+    fun `token exchange request results in SAML token response`() {
+        `when`(serviceClient.doPost(anyString(), anyMap(), anyString()))
+            .thenReturn(SAML_TOKEN_RESPONSE_BODY)
+        `when`(egressTokenGetter.getServiceUserToken()).thenReturn(serviceTokenData())
+
         mvc.perform(
-            get(TOKEN_PATH))
+            post(TOKEN_EXCHANGE_PATH)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer jwt")
+                .content(SAML_TOKEN_REQUEST_BODY))
+            .andExpect(status().isOk)
+            .andExpect(content().json(SAML_TOKEN_RESPONSE_BODY))
+    }
+
+    @Test
+    fun `unauthorized JWT access token request results in response status Unauthorized`() {
+        mvc.perform(
+            get(JWT_ACCESS_TOKEN_PATH))
+            .andExpect(status().isUnauthorized)
+    }
+
+    @Test
+    fun `unauthorized token exchange request results in response status Unauthorized`() {
+        mvc.perform(
+            post(TOKEN_EXCHANGE_PATH)
+                .content(SAML_TOKEN_REQUEST_BODY))
             .andExpect(status().isUnauthorized)
     }
 
     private companion object {
-        const val TOKEN_PATH = "/rest/v1/sts/token"
+        const val BASE_PATH = "/rest/v1/sts/token"
+        const val TOKEN_EXCHANGE_PATH = "$BASE_PATH/exchange"
+        const val JWT_ACCESS_TOKEN_PATH = "$BASE_PATH?grant_type=client_credentials&scope=openid"
+
+        // access_token is a JWT access token
+        @Language("json")
+        private const val JWT_ACCESS_TOKEN_RESPONSE_BODY = """{
+    "access_token": "eyJra...ehtzw",
+    "token_type": "Bearer",
+    "expires_in": 3600
+}"""
+
+        // subject_token is a JWT ID token
+        private const val SAML_TOKEN_REQUEST_BODY = """grant_type: "urn:ietf:params:oauth:grant-type:token-exchange"
+subject_token_type: "urn:ietf:params:oauth:token-type:access_token"
+subject_token: "eyJra...PH5mA""""
+
+        // access_token is a Base64-encoded SAML token
+        @Language("json")
+        private const val SAML_TOKEN_RESPONSE_BODY = """{
+    "access_token": "PHNhb...lvbj4",
+    "issued_token_type": "urn:ietf:params:oauth:token-type:saml2",
+    "token_type": "Bearer",
+    "expires_in": 2649
+}"""
     }
 }
