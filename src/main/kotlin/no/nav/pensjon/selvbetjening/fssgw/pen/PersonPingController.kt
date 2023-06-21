90ba7440863125d6a68906c7e7d48d03f65d105e
Sindre Rasmussen <sindre.rasmussen@nav.no>
2023-06-21 12:24:04 +0200

    Added ping controllers for Behandle alderspensjon and Person

---
+++ src/main/kotlin/no/nav/pensjon/selvbetjening/fssgw/pen/PersonPingController.kt
@@ -0,0 +1,31 @@
+package no.nav.pensjon.selvbetjening.fssgw.pen
+
+import no.nav.pensjon.selvbetjening.fssgw.common.BasicProtectedControllerBase
+import no.nav.pensjon.selvbetjening.fssgw.common.CallIdGenerator
+import no.nav.pensjon.selvbetjening.fssgw.common.ServiceClient
+import no.nav.pensjon.selvbetjening.fssgw.tech.basicauth.BasicAuthValidator
+import no.nav.pensjon.selvbetjening.fssgw.tech.sts.ServiceTokenGetter
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.web.bind.annotation.*
+import java.util.*
+import jakarta.servlet.http.HttpServletRequest
+
+@RestController
+@RequestMapping("pen")
+class PersonPingController(
+    authValidator: BasicAuthValidator,
+    private val egressTokenGetter: ServiceTokenGetter,
+    serviceClient: ServiceClient,
+    callIdGenerator: CallIdGenerator,
+    @Value("\${pen.url}") egressEndpoint: String)
+    : BasicProtectedControllerBase(authValidator, serviceClient, callIdGenerator, egressEndpoint) {
+
+    @GetMapping("api/soknad/api/person/ping")
+    fun handleGetRequest(request: HttpServletRequest)= super.doGet(request)
+
+    override fun provideHeaderAuth(request: HttpServletRequest, headers: TreeMap<String, String>) {
+        val token = egressTokenGetter.getServiceUserToken().accessToken
+        headers[HttpHeaders.AUTHORIZATION] = "Bearer $token"
+    }
+}
