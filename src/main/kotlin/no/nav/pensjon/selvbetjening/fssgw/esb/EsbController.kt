b676655892403ea96e069199ff13128f45897627
Sæbø <gard.sebo@nav.no>
2024-07-04 13:39:59 +0200

    Fjerner logging av body

--- src/main/kotlin/no/nav/pensjon/selvbetjening/fssgw/esb/EsbController.kt
+++ src/main/kotlin/no/nav/pensjon/selvbetjening/fssgw/esb/EsbController.kt
@@ -41,7 +41,6 @@ class EsbController(
     )
     fun handlePostRequest(@RequestBody body: String, request: HttpServletRequest): ResponseEntity<String> {
         logger.info("Request url: ${request.requestURL}")
-        logger.info("Body:\n$body")
         return super.doPost(request, body, useServiceUser2 = false)
 
     }
