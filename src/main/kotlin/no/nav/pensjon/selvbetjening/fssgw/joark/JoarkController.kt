f0293edfa395dc0668beea691e1ac3721ba78c4c
Espen Gjøstøl <espen.oskar.gjostol@nav.no>
2024-09-27 14:48:31 +0200

    Add support for JOARK via wasapp

---
+++ src/main/kotlin/no/nav/pensjon/selvbetjening/fssgw/joark/JoarkController.kt
@@ -0,0 +1,35 @@
+package no.nav.pensjon.selvbetjening.fssgw.joark
+
+import jakarta.servlet.http.HttpServletRequest
+import no.nav.pensjon.selvbetjening.fssgw.common.CallIdGenerator
+import no.nav.pensjon.selvbetjening.fssgw.common.EgressBodyAuthController
+import no.nav.pensjon.selvbetjening.fssgw.common.ServiceClient
+import no.nav.pensjon.selvbetjening.fssgw.tech.jwt.JwsValidator
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class JoarkController(
+    ingressTokenValidator: JwsValidator,
+    serviceClient: ServiceClient,
+    callIdGenerator: CallIdGenerator,
+    @Value("\${wasapp.url}") egressEndpoint: String,
+    @Value("\${sts.password}") private val password: String)
+    : EgressBodyAuthController(ingressTokenValidator, serviceClient, callIdGenerator, egressEndpoint, password) {
+
+    private val logger = LoggerFactory.getLogger(JoarkController::class.java)
+
+    @PostMapping(
+        value = [
+            "joark/Journal",
+            "joark/Journalbehandling"]
+    )
+    fun handlePostRequest(@RequestBody body: String, request: HttpServletRequest): ResponseEntity<String> {
+        logger.info("Request url: ${request.requestURL}")
+        return super.doPost(request, body, serviceUserId = 1)
+    }
+}
